6a2b63ee11f25d4e397137bcd434a0900f1a5756
Fasamii <fasami0512@gmail.com>
2026-03-01 01:45:46 +0100

    minor improvements of epoll bind

--- muxw-bind-epoll/src/lib.rs
+++ muxw-bind-epoll/src/lib.rs
@@ -45,60 +45,70 @@ impl Default for Interest {
 
 impl Interest {
     #[inline]
+    #[must_use]
     pub fn readable(mut self) -> Self {
         self |= Self::READ;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn writable(mut self) -> Self {
         self |= Self::WRITE;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn urgent(mut self) -> Self {
         self |= Self::URGENT;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn error(mut self) -> Self {
         self |= Self::ERROR;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn hang_up(mut self) -> Self {
         self |= Self::HANG_UP;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn closed(mut self) -> Self {
         self |= Self::CLOSED;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn edge_triggered(mut self) -> Self {
         self |= Self::EDGE_TRIGGERED;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn one_shot(mut self) -> Self {
         self |= Self::ONE_SHOT;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn exclusive(mut self) -> Self {
         self |= Self::EXCLUSIVE;
         self
     }
 
     #[inline]
+    #[must_use]
     pub fn wake_up(mut self) -> Self {
         self |= Self::WAKE_UP;
         self
@@ -133,42 +143,49 @@ impl Default for Events {
 impl Events {
     /// Check if the fd is readable
     #[inline]
-    pub fn is_readable(self) -> bool {
+    #[must_use]
+    pub const fn is_readable(self) -> bool {
         self.contains(Self::READABLE)
     }
 
     /// Check if the fd is writable
     #[inline]
-    pub fn is_writable(self) -> bool {
+    #[must_use]
+    pub const fn is_writable(self) -> bool {
         self.contains(Self::WRITABLE)
     }
 
     /// Check if there's urgent data
     #[inline]
-    pub fn is_urgent(self) -> bool {
+    #[must_use]
+    pub const fn is_urgent(self) -> bool {
         self.contains(Self::URGENT)
     }
 
     /// Check if there was an error
     #[inline]
-    pub fn is_error(self) -> bool {
+    #[must_use]
+    pub const fn is_error(self) -> bool {
         self.contains(Self::ERROR)
     }
 
     /// Check if the connection hung up
     #[inline]
-    pub fn is_hang_up(self) -> bool {
+    #[must_use]
+    pub const fn is_hang_up(self) -> bool {
         self.contains(Self::HANG_UP)
     }
 
     /// Check if the peer closed their side
     #[inline]
-    pub fn is_read_closed(self) -> bool {
+    #[must_use]
+    pub const fn is_read_closed(self) -> bool {
         self.contains(Self::READ_CLOSED)
     }
 
     /// Check if the connection is closed (either hang up or read closed)
     #[inline]
+    #[must_use]
     pub fn is_closed(self) -> bool {
         self.intersects(Self::HANG_UP | Self::READ_CLOSED)
     }
@@ -183,7 +200,7 @@ enum CtlOperation {
 
 #[repr(C)]
 #[cfg_attr(target_arch = "x86_64", repr(packed))]
-#[derive(Clone, Copy, PartialEq, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Event {
     config: u32,
     data: u64,
@@ -191,34 +208,53 @@ pub struct Event {
 
 impl Event {
     #[inline]
-    pub fn blank() -> Self {
+    #[must_use]
+    pub const fn blank() -> Self {
         Self { config: 0, data: 0 }
     }
 
     #[inline]
-    pub fn fd(&self) -> RawFd {
+    #[must_use]
+    #[allow(clippy::cast_possible_truncation)]
+    pub const fn fd(&self) -> RawFd {
         self.data as RawFd
     }
 
     #[inline]
-    pub fn events(self) -> Events {
+    #[must_use]
+    pub const fn events(self) -> Events {
         Events::from_bits_truncate(self.config)
     }
 }
 
+/// # Errors
+/// Returns error when epoll failed to create
+/// The fallout lies on the system side not caller
 pub fn create(cloexec: bool) -> Result<RawFd> {
     let flags = if cloexec { libc::EPOLL_CLOEXEC } else { 0 };
     unsafe { ok_or_get_error(libc::epoll_create1(flags)) }
 }
 
+/// # Errors
+/// Returns `Err(io::Error)` when:
+/// - You provided invalid `epoll_fd`
+/// - You provided invalid listener fd
 pub fn add_fd(epoll_fd: RawFd, fd: RawFd, interest: Interest) -> Result<()> {
     ctl(epoll_fd, CtlOperation::Add, fd, interest)
 }
 
+/// # Errors
+/// Returns `Err(io::Error)` when:
+/// - You provided invalid `epoll_fd`
+/// - You provided invalid listener fd
 pub fn mod_fd(epoll_fd: RawFd, fd: RawFd, interest: Interest) -> Result<()> {
     ctl(epoll_fd, CtlOperation::Mod, fd, interest)
 }
 
+/// # Errors
+/// Returns `Err(io::Error)` when:
+/// - You provided invalid `epoll_fd`
+/// - You provided invalid listener fd
 pub fn del_fd(epoll_fd: RawFd, fd: RawFd) -> Result<()> {
     ctl(epoll_fd, CtlOperation::Del, fd, Interest::empty())
 }
@@ -226,31 +262,51 @@ pub fn del_fd(epoll_fd: RawFd, fd: RawFd) -> Result<()> {
 fn ctl(epoll_fd: RawFd, operation: CtlOperation, fd: RawFd, interest: Interest) -> Result<()> {
     let mut config = libc::epoll_event {
         events: interest.bits(),
+        #[allow(clippy::cast_sign_loss)]
         u64: fd as u64,
     };
-    unsafe { ok_or_get_error(libc::epoll_ctl(epoll_fd, operation as i32, fd, &mut config))? };
+    unsafe {
+        ok_or_get_error(libc::epoll_ctl(
+            epoll_fd,
+            operation as i32,
+            fd,
+            &raw mut config,
+        ))?
+    };
     Ok(())
 }
 
+/// # Errors
+/// Returns `Err(io::Error)` when:
+/// - You provided invalid `epoll_fd`
+///
+/// # Panics
+///
 pub fn wait(epoll_fd: RawFd, timeout: Option<i32>, buf: &mut [Event]) -> Result<usize> {
     let timeout = timeout.unwrap_or(-1);
 
     let sys_buf = unsafe {
-        std::slice::from_raw_parts_mut(buf.as_mut_ptr() as *mut libc::epoll_event, buf.len())
+        std::slice::from_raw_parts_mut(buf.as_mut_ptr().cast::<libc::epoll_event>(), buf.len())
     };
 
+    let sys_buf_len = i32::try_from(sys_buf.len()).unwrap_or(i32::MAX);
+
+    #[allow(clippy::expect_used)]
     let n = unsafe {
-        ok_or_get_error(libc::epoll_wait(
+        usize::try_from(ok_or_get_error(libc::epoll_wait(
             epoll_fd,
             sys_buf.as_mut_ptr(),
-            sys_buf.len() as i32,
+            sys_buf_len,
             timeout,
-        ))? as usize
+        ))?)
+        // TODO: create some ok_or_get_error which returns usize instead of c_int
+        .unwrap_or_else(|_| unreachable!("ok_or_get_error guarantees value is >= 0"))
     };
 
     Ok(n)
 }
 
+#[allow(clippy::missing_errors_doc)]
 pub fn close(epoll_fd: RawFd) -> Result<()> {
     ok_or_get_error(unsafe { libc::close(epoll_fd) })?;
     Ok(())
