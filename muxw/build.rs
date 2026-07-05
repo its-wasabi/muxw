6e88088b58e56b7383f3add7d2253c428c1dcabf
wasabi <fasami0512@gmail.com>
2026-07-06 00:10:04 +0200

    Refactor shell completions build script

--- muxw/build.rs
+++ muxw/build.rs
@@ -7,28 +7,27 @@ use std::io::Error;
 mod domain;
 
 fn main() -> Result<(), Error> {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
+    if let Some(outdir) = env::var_os("OUT_DIR") {
+        let mut cmd = domain::Cli::command();
+        let bin_name = env!("CARGO_PKG_NAME");
 
-    let mut cmd = domain::Cli::command();
-    let bin_name = env!("CARGO_PKG_NAME");
+        let shells = [
+            Shell::Bash,
+            Shell::Zsh,
+            Shell::Fish,
+            Shell::Elvish,
+            Shell::PowerShell,
+        ];
 
-    let shells = [
-        Shell::Bash,
-        Shell::Zsh,
-        Shell::Fish,
-        Shell::Elvish,
-        Shell::PowerShell,
-    ];
+        for shell in shells {
+            generate_to(shell, &mut cmd, bin_name, &outdir)?;
+        }
 
-    for shell in shells {
-        generate_to(shell, &mut cmd, bin_name, &outdir)?;
+        println!(
+            "cargo:info=Completion scripts generated in {}",
+            outdir.display()
+        );
     }
-
-    // TODO: find nicer way to display that (not as warning)
-    println!("cargo:warning=Completion scripts generated in {:?}", outdir);
 
     Ok(())
 }
--- muxw/default_config.rs
+++
@@ -1,9 +0,0 @@
-pub const DEFAULT_CONFIG: &str = /* lua */
-    r#"
-print("INSIDE LUA")
-Mux.bind("W", Mux.motion.focus.up);
-Mux.bind("S", Mux.motion.focus.down);
-Mux.bind("D", Mux.motion.focus.right);
-Mux.bind("A", Mux.motion.focus.left);
-print("LUA DONE")
-"#;
